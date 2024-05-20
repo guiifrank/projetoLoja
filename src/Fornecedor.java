943e6f0762094174abfb3820d958f632b710505c
guiifrank <gsusinfrank@gmail.com>
2024-05-20 19:23:52 -0300

    Dado inicio ao menu de escolha do usuario com switch case na classe Main

--- src/Fornecedor.java
+++ src/Fornecedor.java
@@ -4,4 +4,5 @@ public class Fornecedor {
     private String telefone;
     private String email;
 
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite o número correspondente ao que deseja fazer:");
+        System.out.println("1) Cadastrar um fornecedor");
+        System.out.println("2) Cadastrar um produto");
+        System.out.println("3) Gerenciar estoque de produtos");
+        System.out.println("4) Sair");
 
+        int opcao = sc.nextInt();
+
+        switch (opcao) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
 
         sc.close();
     }
