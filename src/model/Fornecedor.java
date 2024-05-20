c0ccf9f77e19652de2ee87a5676629fe8bfeecb0
guiifrank <gsusinfrank@gmail.com>
2024-05-20 20:09:09 -0300

    Criado construtores para classes Produto, Fornecedor, Estoque

--- src/model/Fornecedor.java
+++ src/model/Fornecedor.java
@@ -1,19 +1,34 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fornecedor {
     private String name;
     private String descricao;
     private String telefone;
     private String email;
+    private int id;  //USUARIO PODE SETAR ID? OU SISTEMA SETA ID AO INLCUIR FORNECEDOR?
+
+    private List<Fornecedor> fornecedores = new ArrayList<>();
 
     public Fornecedor() {
     }
 
-    public Fornecedor(String name, String descricao, String telefone, String email) {
+    public Fornecedor(String name, String descricao, String telefone, String email, int id) {
         this.name = name;
         this.descricao = descricao;
         this.telefone = telefone;
         this.email = email;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
